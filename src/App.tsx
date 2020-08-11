3d46ce0e74f066ca0ac9b5d9cfb43dd3aa3e54cd
Hailey Smith <hsmitty93@gmail.com>
2020-08-11 16:52:21 -0700

    add scrolltrigger to app bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
-//Material Components
+// Components
+/* import Lottie from 'react-lottie-player'; */
 import {
   AppBar,
   Toolbar,
@@ -8,50 +9,64 @@ import {
   Typography,
   useScrollTrigger,
 } from '@material-ui/core';
-
-//Material Icons
 import MenuIcon from '@material-ui/icons/Menu';
+
+// Assets
+/* import animationData from './assets/welcome_animation.json'; */
 
 // Style
 import './App.css';
 
 interface Props {
   children?: any;
+  trigger?: any;
 }
 
 function ScrollTrigger(props: Props) {
-  const { children } = props;
-  const trigger = useScrollTrigger({
-    threshold: 10
-  });
+  const { children, trigger } = props;
+  
 
   return React.cloneElement(children, {
     elevation: trigger ? 4: 0,
     style: {
-      height: trigger ? 64 : 90,
-      transition: trigger ? "0.4s ease-in" : "0.4s ease-out",
+      minHeight: trigger ? 64 : "100vh",
+      transition: trigger ? "0.3s ease-in" : "1s ease-out",      
     }
     
   })
 }
 
 export default function App(props: Props) {
+
+  const trigger = useScrollTrigger({
+    threshold: 20
+  });
+
   return (
     <div className="app">
-      <ScrollTrigger {...props}>
-        <AppBar className="app-bar">
-          <Toolbar>
-            <Typography variant="h5" className="app-bar__title">
-              Hailey Smith
-            </Typography>
-            <IconButton edge="end" className="app-bar__menu-icon" aria-label="menu">
-              <MenuIcon/>
-            </IconButton>
-          </Toolbar>
-        </AppBar>
+      <ScrollTrigger trigger={trigger} {...props}>
+        <AppBar className={trigger ? "app-bar" : "bg-intro"}>
+          <div className="bg-intro" style={{visibility: trigger ? "hidden" : "visible", minHeight: trigger ? 0 : "100vh", width: trigger ? 0 : "100%"}}>
+            {!trigger && 
+              <Typography variant="h2" className="bg-intro__title">
+                Hello, it's nice to meet you!
+              </Typography>
+            }
+          </div>
+          {trigger && 
+            <Toolbar>
+              <Typography variant="h5" className="app-bar__title">
+                Hailey Smith
+              </Typography>
+              <IconButton edge="end" className="app-bar__menu-icon" aria-label="menu">
+                <MenuIcon/>
+              </IconButton>
+            </Toolbar> 
+          }
+        </AppBar> 
       </ScrollTrigger>
       <div className="test">
-
+        
       </div>
     </div>
     // header
